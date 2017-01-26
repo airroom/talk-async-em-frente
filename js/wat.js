343b2c606b6a8ead65b9219134b2f3917e904532
Romário S. Lima <r.lima616@gmail.com>
2017-01-26 06:42:46 -0200

    some stlying

---
+++ js/wat.js
@@ -0,0 +1,55 @@
+(function() {
+  var throttle = function(type, name, obj) {
+    obj = obj || window;
+    var running = false;
+    var func = function() {
+        if (running) { return; }
+        running = true;
+         requestAnimationFrame(function() {
+            obj.dispatchEvent(new CustomEvent(name));
+            running = false;
+        });
+    };
+    obj.addEventListener(type, func);
+  };
+
+  /* init - you can init any event */
+  throttle("resize", "optimizedResize");
+})();
+
+(function () {
+  var l = console.log;
+  var boardSign = document.querySelector('.main-board-sign');
+  var baseSizing = { width: 1600, height: 1000 };
+  var ratio = baseSizing.width / baseSizing.height;
+  var boardSignSize = 603;
+  var originalCoords = { x: 0.4125, y: 0.08  };
+
+  var setBoardSize = () => {
+    var innerWidth = window.innerWidth;
+    var innerHeight = window.innerHeight;
+    var currentRatio = innerWidth / innerHeight;
+    var currentWidth, currentHeight, currentBoardSize;
+
+    if (currentRatio > ratio) {
+      currentWidth = innerWidth;
+      currentHeight = innerWidth / ratio;
+    } else {
+      currentWidth = innerHeight * ratio;
+      currentHeight = innerHeight;
+    }
+
+    currentBoardSize = (currentWidth * boardSignSize) / baseSizing.width;
+
+    boardSign.style.top = currentHeight * originalCoords.y + 'px';
+    boardSign.style.left = currentWidth * originalCoords.x + 'px';
+    boardSign.style.width = currentBoardSize + 'px';
+    boardSign.style.height = currentBoardSize + 'px';
+  };
+
+
+  setBoardSize();
+
+  window.addEventListener("optimizedResize", setBoardSize);
+
+})();
